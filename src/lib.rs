8be1293f33f0572ad26f1bdc03eeeb2234adfd00
Kaydax <kaydax@kaydax.xyz>
2023-04-25 16:01:46 -0400

    Fix compile for windows

--- src/lib.rs
+++ src/lib.rs
@@ -329,7 +329,7 @@ impl FileDialog {
         })
         .map(|entry| {
           let mut file_name = entry.file_name().into_string().unwrap();
-          #[cfg(unix)]
+          #[cfg(linux)]
           if file_name.starts_with('.') {
             self.hidden_files += 1;
           }
